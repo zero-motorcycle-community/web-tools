31d467bdfea95f3aaf4e0a970a39eabf10574f0c
Greg Hassler <greg@hasslers.com>
2017-01-24 23:45:53 -0500

    Update vin_decoder.php

--- vindecoder/vin_decoder.php
+++ vindecoder/vin_decoder.php
@@ -341,34 +341,34 @@ function decode_plant($zplant){
 
 
 function validate_vin($vin) {
-    $vin = strtolower($vin);
-    if (!preg_match('/^[^\ioq]{17}$/', $vin)) { 
+	$vin = strtolower($vin);
+	if (!preg_match('/^[^\ioq]{17}$/', $vin)) { 
 		// not valid if not 17 characters or has I, O, or Q in VIN
-        return false;
-    }
-    $weights = array(8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2);
-    $transliterations = array(
+		return false;
+	}
+	$weights = array(8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2);
+	$transliterations = array(
 		"a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6, "g" => 7, "h" => 8,
 		"j" => 1, "k" => 2, "l" => 3, "m" => 4, "n" => 5, "p" => 7, "r" => 9, "s" => 2,
 		"t" => 3, "u" => 4, "v" => 5, "w" => 6, "x" => 7, "y" => 8, "z" => 9
-    );
-    $sum = 0;
-    for($i = 0 ; $i < strlen($vin) ; $i++ ) { // loop through characters of VIN
-        // add transliterations * weight of their positions to get the sum
-        if(!is_numeric($vin{$i})) {
-            $sum += $transliterations[$vin{$i}] * $weights[$i];
-        } else {
-            $sum += $vin{$i} * $weights[$i];
-        }
-    }
-    // find check digit by taking the mod of the sum
-    $checkdigit = $sum % 11;
-    if($checkdigit == 10) { // check digit of 10 is represented by "X"
-        $checkdigit = "x";
-    }
-	
+	);
+	$sum = 0;
+	for($i = 0 ; $i < strlen($vin) ; $i++ ) { // loop through characters of VIN
+		// add transliterations * weight of their positions to get the sum
+		if(!is_numeric($vin{$i})) {
+			$sum += $transliterations[$vin{$i}] * $weights[$i];
+		} else {
+			$sum += $vin{$i} * $weights[$i];
+		}
+	}
+	// find check digit by taking the mod of the sum
+	$checkdigit = $sum % 11;
+	if($checkdigit == 10) { // check digit of 10 is represented by "X"
+		$checkdigit = "x";
+	}
+
 	//will return true or false, based on if calculated check digit matches 9th digit of VIN
-    return var_export($checkdigit == $vin{8}, true);
+	return var_export($checkdigit == $vin{8}, true);
 }
 
 ?>
