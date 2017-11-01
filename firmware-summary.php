bd9e960c264d35e0fda8092884dfa1f6d03f7e26
Greg Hassler <greg@hasslers.com>
2017-10-31 22:32:41 -0400

    Mobile friendly-ness

--- firmware-summary.php
+++ firmware-summary.php
@@ -3,6 +3,7 @@
 <head>
 	<title>Latest Firmwares Found</title>
 	<link rel="stylesheet" type="text/css" href="style.css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<style type="text/css">
 		body{
 			font-family: Consolas, Lucida Console, Courier;
