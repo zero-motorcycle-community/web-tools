e903d13f627941f1e6b873b7db45a111e59a38c6
Greg Hassler <greg@hasslers.com>
2017-10-31 22:35:54 -0400

    Mobile friendly-ness

--- vindecoder/index.php
+++ vindecoder/index.php
@@ -3,6 +3,7 @@
 <head>
 	<title>Zero Motorcycles Online VIN Decoder</title>
 	<link rel="stylesheet" type="text/css" href="../style.css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 
